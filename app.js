6e87f43d9c0b00076b76c680694bcff56d276eee
Harry Ramli <39229239+sourceleak@users.noreply.github.com>
2019-01-22 18:35:27 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ var stockCode = process.argv[2];
 
 try {
     https.get(`https://api.iextrading.com/1.0/stock/${stockCode}/stats`, (response) => {
-    var responseBody = "";
+    var stockStats = "";
+    var stockPrice = "";
     response.on('data', data => {
-        responseBody+=data;
+        stockStats+=data;
     })
     response.on('end', () => {
         try {
-            responseBody = responseBody.toString();
-            var jsonData = JSON.parse(responseBody);
-            console.log(`${jsonData.companyName}'s stock price:\nhigh: ${jsonData.week52high}\nlow: ${jsonData.week52low}`)
-        } catch {
-            console.log('Unknown symbol');
+            stockStats = stockStats.toString();
+            var jsonStockData = JSON.parse(stockStats);
+            https.get(`https://api.iextrading.com/1.0/stock/${stockCode}/price`, (response)=>{
+                response.on('data',data=> {
+                    stockPrice+=data;
+                })
+                response.on('end', ()=>{
+                    try{
+                        stockPrice = stockPrice.toString();
+                        var jsonPriceData = JSON.parse(stockPrice);
+                        console.log(`${jsonStockData.companyName}: ${stockPrice}\nHigh: ${jsonStockData.week52high}\nLow: ${jsonStockData.week52low}`)
+                    }
+                    catch (error) {
+                        console.log(error.message);
+                    }
+                })
+            });
+        
+        } catch (error) {
+            if(error.code != undefined)
+                console.log('HTTP request error.');
+            else 
+                console.log('Unknown symbol');
         }
     })
 });
